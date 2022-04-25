18313459ac317d28e7d26b7b0e8c890daa3d319b
OliaChuzhdiuk <90050382+OliaChuzhdiuk@users.noreply.github.com>
2022-04-25 22:26:18 +0300

    add

---
+++ src/js/main.js
@@ -0,0 +1,87 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import ServiceAPI from './service';
+import markup from './markup';
+
+const form = document.querySelector('.search-form');
+const searchButton = document.querySelector('[type=submit]');
+const gallery = document.querySelector('.gallery');
+
+const options = {
+    simpleLightBox: {
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+    },
+    intersectionObserver: {
+        root: null,
+        threshold: 1,
+    },
+};
+
+const loadService = new ServiceAPI();
+
+form.addEventListener('submit', onFormSubmit);
+
+const callback = function(entries, observer) {
+    if (entries[0].isIntersecting) {
+        observer.unobserve(entries[0].target);
+        loadPictures();
+    }
+};
+const observer = new IntersectionObserver(callback, options.intersectionObserver);
+
+let galleryLightBox = new SimpleLightbox('.gallery a', options.simpleLightBox);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    const isFilled = e.currentTarget.elements.searchQuery.value;
+    if (isFilled) {
+        searchButton.disabled = true;
+        loadService.searchQuery = isFilled;
+        loadService.resetPage();
+        gallery.innerHTML = '';
+        loadPictures();
+    }
+}
+
+function loadPictures() {
+    loadService
+        .getPictures()
+        .then(dataProcessing)
+        .catch(error => {
+            console.log(error);
+            Notify.failure('Something went wrong, please try again...');
+        });
+}
+
+function dataProcessing(data) {
+    searchButton.disabled = false;
+    if (data.data.totalHits === 0) {
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        return;
+    }
+    if (data.data.totalHits !== 0 && data.data.hits.length === 0) {
+        Notify.warning(`We're sorry, but you've reached the end of search results.`);
+        return;
+    }
+
+    gallery.insertAdjacentHTML('beforeend', markup(data.data.hits));
+
+    galleryLightBox.refresh();
+
+    if (loadService.pageNumber === 2) {
+        Notify.info(`Hooray! We found ${data.data.totalHits} images.`);
+    } else {
+        const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
+        window.scrollBy({
+            top: cardHeight * 2 + 120,
+            behavior: 'smooth',
+        });
+    }
+    observer.observe(gallery.lastElementChild);
+}
